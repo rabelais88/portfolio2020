feabec23df1d253cd5d574bfacedf38a8e1a340a
max <sungryeolp@gmail.com>
2020-07-01 12:22:06 +0900

    :bug: fixed tag related bugs

--- api/control/tag.go
+++ api/control/tag.go
@@ -3,37 +3,53 @@ package control
 import (
 	"net/http"
 
+	"strconv"
+
 	"github.com/labstack/echo/v4"
 	"github.com/rabelais88/portfolio2020/api/env"
 )
 
 type BrowseTagsQuery struct {
 	Keyword string `query:"keyword"`
+	Limit   string `query:"limit" validate:"omitempty,numeric"`
 }
 
 type BrowseTagsResponseItem struct {
-	TagValue string `json:"tagValue"`
-	Count    int    `json:"count"` // article count
+	Tag          string `json:"tag"`
+	ArticleCount int    `json:"articleCount"` // article count
 }
 
 type BrowseTagsResponse struct {
 	Tags []BrowseTagsResponseItem `json:"tags"`
 }
 
+// browse tags produced by articles
+// GET /tags?keyword=$TARGET_TAG&limit=10
 func BrowseTags(c echo.Context) error {
 	cc := c.(*env.CustomContext)
 
-	b := new(BrowseTagsQuery)
-	if err := cc.Bind(b); err != nil {
+	q := new(BrowseTagsQuery)
+	if err := cc.Bind(q); err != nil {
 		return MakeError(http.StatusBadRequest, "QUERY_NOT_READABLE")
 	}
 
-	// SELECT tag_value, COUNT(article_id) FROM public.article_tags GROUP BY tag_value ORDER BY COUNT(article_id) DESC;
-	// method 1 - does work, but the count becomes 0 on in-memory mode.
+	if err := cc.Validate(q); err != nil {
+		return err
+	}
+
+	// 1. do not use COUNT() as upper case letter.
+	// 2. reassign count columns if possible(different db causes different count column name)
 	ts := []BrowseTagsResponseItem{}
-	db := cc.Db.Table("article_tags").Select("tag_value, COUNT(article_id)").Group("tag_value").Order("COUNT(article_id) desc").Limit(10)
-	if b.Keyword != "" {
-		db = db.Where("tag_value LIKE ?", b.Keyword+"%")
+	db := cc.Db.Table("article_tags").Select("tag_value as tag, count(article_id) as article_count").Group("tag_value").Order("count(article_id) desc")
+	if q.Keyword != "" {
+		db = db.Where("tag_value LIKE ?", q.Keyword+"%")
+	}
+	if q.Limit != "" {
+		lim, err := strconv.Atoi(q.Limit)
+		if err != nil {
+			return MakeError(http.StatusBadRequest, "QUERY_NOT_READABLE")
+		}
+		db = db.Limit(lim)
 	}
 	db.Scan(&ts)
 
