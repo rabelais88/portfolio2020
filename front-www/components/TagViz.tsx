f8206a46a33e9e22c2b6bf3852b0e0a6e1dacf48
sungryeol <sungryeolp@gmail.com>
2020-07-15 01:54:38 +0900

    :lipstick: fixed styling issues of d3 component

--- front-www/components/TagViz.tsx
+++ front-www/components/TagViz.tsx
@@ -1,7 +1,11 @@
+import Logger from 'lib/logger';
 import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
-import { setSize, ttrans } from 'd3-quicktool';
+import { setSize, move } from 'd3-quicktool';
 import { tag } from 'types/tag';
+import checkClient from 'lib/checkClient';
+
+const logger = new Logger('components/TagViz.tsx');
 
 interface TagVizProps {
   tags: tag[];
@@ -9,6 +13,7 @@ interface TagVizProps {
   textMargin?: number;
   marginX?: number;
   marginY?: number;
+  threshold?: number;
 }
 
 interface getConfigArgs extends TagVizProps {
@@ -22,6 +27,7 @@ const getConfig = ({
   marginX,
   marginY,
   textMargin,
+  threshold,
 }: getConfigArgs) => ({
   width,
   height: tags.length * barHeight + marginY * 2,
@@ -29,29 +35,106 @@ const getConfig = ({
   marginY,
   textMargin,
   barHeight,
+  threshold,
 });
 
-const TagViz: React.FunctionComponent<TagVizProps> = (props) => {
-  const containerRef = useRef(null);
-  const [svg, setSvg] = useState(null);
-  const [config, setConfig] = useState(getConfig({ ...props, width: 500 }));
-  useEffect(() => {
-    const elContainer = containerRef.current || {};
-    setConfig(getConfig({ ...props, width: elContainer.offsetWidth || 0 }));
-  }, [props]);
-  useEffect(() => {
-    // when component is mounted
-    if (!svg) {
-      const _svg = d3.select('#tagViz').append('svg');
-      setSvg(_svg);
+const drawDot = (dotRadius) => {
+  return function (d, i, e) {
+    for (let di = 0; di < d.articleCount; di += 1) {
+      d3.select(this)
+        .append('circle')
+        .attr('r', dotRadius / 2)
+        .attr('fill', 'red')
+        .call(move, () => [dotRadius * di + dotRadius / 2, dotRadius / 2]);
     }
-  }, [containerRef.current]);
-  if (svg) {
+  };
+};
+
+const TagViz: React.FunctionComponent<TagVizProps> = (props) => {
+  const { tags, threshold } = props;
+  const svgRef = useRef(null);
+  const containerRef = useRef(null);
+  const [viewWidth, setViewWidth] = useState(100);
+
+  useEffect(() => {
+    const handleResize = () => {
+      logger.log(containerRef.current.offsetWidth);
+      setViewWidth(containerRef.current.offsetWidth);
+    };
+    if (checkClient() && svgRef.current && containerRef.current) {
+      handleResize();
+      window.addEventListener('resize', handleResize);
+    }
+  }, [svgRef.current, containerRef.current]);
+  useEffect(() => {
+    const elContainer = svgRef.current || {};
+    const config = getConfig({
+      ...props,
+      width: viewWidth,
+    });
+    // when component is mounted
+    const svg = d3.select(elContainer);
+    logger.log('updating', { width: config.width });
+    const _tags = tags.sort((a, b) => b.articleCount - a.articleCount);
+    const countMax = d3.max(tags, (d) => d.articleCount);
+    const countMin = d3.min(tags, (d) => d.articleCount);
+    const scaleX = d3
+      .scaleLinear()
+      .domain([countMin, countMax])
+      .range([0, config.width - config.marginX * 2]);
+
+    const scaleY = d3
+      .scaleBand<number>()
+      .domain(_tags.map((_, i) => i))
+      .range([0, config.height])
+      .paddingInner(0.2);
+    // changing config triggers redrawing
     svg.call(setSize, config.width, config.height);
-  }
+
+    const updateLargeDataCoord = (el) =>
+      el
+        .attr('width', (d) => scaleX(d.articleCount))
+        .attr('height', (d) => scaleY.bandwidth())
+        .attr('fill', 'black');
+
+    const forLargeData = (el) =>
+      el
+        .filter((d) => d.articleCount >= threshold)
+        .append('rect')
+        .call(updateLargeDataCoord);
+
+    const dotRadius = scaleY.step();
+    const forSmallData = (el) =>
+      el.filter((d) => d.articleCount < threshold).each(drawDot(dotRadius));
+
+    const enterData = (el) =>
+      el
+        .append('g')
+        .attr('class', 'tag-node')
+        .call(move, (_, i) => [0, scaleY(i)])
+        .call(forLargeData)
+        .call(forSmallData);
+
+    const updateData = (el) =>
+      el
+        .filter((d) => d.articleCount >= threshold)
+        .select('rect')
+        .call(updateLargeDataCoord)
+        .attr('data-test', (d, i, e) => {
+          logger.log(e);
+        });
+
+    const exitData = (el) => el.remove();
+
+    svg
+      .selectAll('.tag-node')
+      .data(_tags, (d: tag) => d.tag)
+      .join(enterData, updateData, exitData);
+  }, [svgRef.current, props, viewWidth]);
+
   return (
-    <div ref={containerRef} id="tagViz">
-      &nbsp;
+    <div ref={containerRef}>
+      <svg ref={svgRef} id="tagViz" />
     </div>
   );
 };
@@ -61,6 +144,7 @@ TagViz.defaultProps = {
   marginY: 5,
   textMargin: 100,
   barHeight: 10,
+  threshold: 3,
 };
 
 export default TagViz;
---
+++ front-www/stories/tagviz.stories.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import TagViz from '../components/TagViz';
+import sampleTags from './sampleTags.json';
+
+export default { title: 'TagViz', decorators: [withKnobs] };
+
+export const defaultData = () => <TagViz tags={sampleTags} />;
+f
