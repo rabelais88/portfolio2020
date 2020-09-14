0f37839af3c9ab4d57f89f51055d63383c226393
sungryeol <sungryeolp@gmail.com>
2020-09-14 20:16:03 +0900

    :bug: fixed URL validator bug

--- front-admin/src/validateRules.js
+++ front-admin/src/validateRules.js
@@ -5,14 +5,10 @@ import { extend } from 'vee-validate';
 /* eslint-disable operator-linebreak */
 function validURL(str) {
   const pattern = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
-    'i',
-  ); // fragment locator
+    // eslint-disable-next-line
+    `[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)`,
+    'gi',
+  );
   return !!pattern.test(str);
 }
 
